d2167be257eb3c46481527932f72e413191b664a
sakumafuminori <sakumafuminori@gmail.com>
2021-05-27 17:49:45 +0900

    初回コミット

---
+++ contactform/insert.php
@@ -0,0 +1,29 @@
+<?php
+mb_internal_encoding("utf8");
+
+$pdo = new PDO("mysql:dbname=lesson01;host=localhost;","root","mysql");
+
+$pdo->exec("insert into contactform(name,mail,age,comment)
+values('".$_POST['name']."','".$_POST['mail']."','".$_POST['age']."','".$_POST['comment']."');");
+
+?>
+
+<!doctype HTML>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <title>お問い合わせフォーム</title>
+    <link rel="stylesheet" type="text/css" href="style2.css">
+</head>
+
+<h1>お問い合わせフォーム</h1>
+
+<div class="confirm">
+    <p>お問い合わせ有難うございました。<br>3営業日以内に担当者よりご連絡差し上げます。
+    </p>
+
+</div>
+
+</body>
+</html>
